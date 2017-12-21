1fca61efde9befed15b2b5968c9278d23426eea5
Tin Nguyen <tin.nguyen1@ge.com>
2017-12-21 17:12:21 -0500

    #14 reorder exercise component

--- src/views/exercises/exercise.js
+++ src/views/exercises/exercise.js
@@ -29,67 +29,6 @@ export default class Exercise extends React.Component {
 		this._updateType = this._updateType.bind(this);
 	}
 
-	_updateName(name) {
-		if (name === '') {
-			return;
-		}
-		this.setState(prevState => {
-			prevState.exercise.displayName = name;
-			if (prevState.exercise.id === undefined) {
-				prevState.exercise.name = name.toLowerCase().replace(/\s/g, '_');
-			}
-			return prevState;
-		}, this._updateExercise);
-	}
-
-	_updateExercise() {
-		if (this.state.exercise.displayName === '') {
-			Alert.alert(
-				'',
-				'Name is required',
-				[
-					{
-						text: 'Ok'
-					}
-				],
-				{ cancelable: false }
-			);
-			return;
-		}
-		if (this.state.exercise.id !== undefined) {
-			localData.saveExercise(this.state.exercise);
-		}
-	}
-
-	_deleteExercise() {
-		ActionSheetIOS.showActionSheetWithOptions(
-			{
-				message: 'Are you sure you want to delete this exercise?',
-				options: ['Delete', 'Cancel'],
-				destructiveButtonIndex: 0,
-				cancelButtonIndex: 1
-			},
-			buttonIndex => {
-				if (buttonIndex === 0) {
-					localData.deleteExercise(this.state.exercise);
-					this.props.navigator.pop();
-				}
-			}
-		);
-	}
-
-	_createExercise() {
-		localData.saveExercise(this.state.exercise);
-		this.props.navigator.pop();
-	}
-
-	_updateType(type) {
-		this.setState(prevState => {
-			prevState.exercise.type = type;
-			return prevState;
-		}, this._updateExercise);
-	}
-
 	render() {
 		return (
 			<ScrollView style={styles.exerciseView} keyboardDismissMode="on-drag">
@@ -138,6 +77,67 @@ export default class Exercise extends React.Component {
 				</View>
 			</ScrollView>
 		);
+	}
+
+	_updateName(name) {
+		if (name === '') {
+			return;
+		}
+		this.setState(prevState => {
+			prevState.exercise.displayName = name;
+			if (prevState.exercise.id === undefined) {
+				prevState.exercise.name = name.toLowerCase().replace(/\s/g, '_');
+			}
+			return prevState;
+		}, this._updateExercise);
+	}
+
+	_updateType(type) {
+		this.setState(prevState => {
+			prevState.exercise.type = type;
+			return prevState;
+		}, this._updateExercise);
+	}
+
+	_updateExercise() {
+		if (this.state.exercise.displayName === '') {
+			Alert.alert(
+				'',
+				'Name is required',
+				[
+					{
+						text: 'Ok'
+					}
+				],
+				{ cancelable: false }
+			);
+			return;
+		}
+		if (this.state.exercise.id !== undefined) {
+			localData.saveExercise(this.state.exercise);
+		}
+	}
+
+	_deleteExercise() {
+		ActionSheetIOS.showActionSheetWithOptions(
+			{
+				message: 'Are you sure you want to delete this exercise?',
+				options: ['Delete', 'Cancel'],
+				destructiveButtonIndex: 0,
+				cancelButtonIndex: 1
+			},
+			buttonIndex => {
+				if (buttonIndex === 0) {
+					localData.deleteExercise(this.state.exercise);
+					this.props.navigator.pop();
+				}
+			}
+		);
+	}
+
+	_createExercise() {
+		localData.saveExercise(this.state.exercise);
+		this.props.navigator.pop();
 	}
 }
 
